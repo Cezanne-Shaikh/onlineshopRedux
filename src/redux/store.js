4a01fbe12ebd7bf28fed6acaac653a40044b6349
cezaass <cezanneshaikh50@gmail.com>
2023-08-21 14:39:10 +0530

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+
+import reducers from './reducers/index';
+import { legacy_createStore } from 'redux';
+
+const store=legacy_createStore(reducers,{},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
